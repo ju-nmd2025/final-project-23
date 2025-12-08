0c3690bfb2a7024835d30cb3c877706f955c637a
ngle25gd <ngle25gd@student.ju.se>
2025-12-08 14:20:31 +0100

    change character

--- character.js
+++ character.js
@@ -5,33 +5,45 @@ export default class Character {
 		this.y = y;
 		this.w = w;
 		this.h = h;
+        this.headRadius = w * 0.75;
+        this.color = 'orange';
 //		this.isOnPlatForm = false;
 	}
 
-    draw() {
-        fill ("orange");
-        rect(this.x, this.y, this.w, this.h);
+    draw() {const cat = {
+            color: this.color,
+            head: { x: this.x, y: this.y, radius: this.headRadius },
+            body: { x: this.x, y: this.y + this.headRadius / 2, w: this.w, h: this.h },
+            legs: [
+                { x: this.x - this.w / 2, y: this.y + this.h, w: 10, h: 15 },
+                { x: this.x + this.w / 2 - 10, y: this.y + this.h, w: 10, h: 15 },
+                { x: this.x - this.w / 2, y: this.y + this.h - 10, w: 10, h: 15 },
+                { x: this.x + this.w / 2 - 10, y: this.y + this.h - 10, w: 10, h: 15 },
+            ],
+            tail: { fromX: this.x + this.w / 2, fromY: this.y + this.h, toX: this.x + this.w / 2 + 10, toY: this.y + this.h + 40 },
+            eyes: [
+                { x: this.x - 10, y: this.y + 10 },
+                { x: this.x + 10, y: this.y + 10 },
+            ],
+            nose: { x: this.x, y: this.y + 15 },
+            ears: [
+                { x1: this.x - 20, y1: this.y, x2: this.x - 5, y2: this.y - 15, x3: this.x, y3: this.y },
+                { x1: this.x + 20, y1: this.y, x2: this.x + 5, y2: this.y - 15, x3: this.x, y3: this.y },
+            ],
+        };
 
-        //ears
-        fill ("darkorange");
-        triangle (this.x, this.y, this.x + 10, this.y - 10, this.x + 20, this.y);
-        triangle (this.x + this.w - 20, this.y, this.x + this.w - 10, this.y - 10, this.x + this.w, this.y);
-
-        // eyes
-        fill ("white");
-        rect (this.x + 10, this.y + 10, 10, 10);
-        rect (this.x + 30, this.y + 10, 10, 10);
-
-        fill ("black");
-        rect (this.x + 12, this.y + 12, 6, 6);
-        rect (this.x + 32, this.y + 12, 6, 6);
+        return cat;
     }
+}
 
-    isColliding(character, platform) {
+// Test
+const myCat = new Character(50, 50, 40, 40);
+console.log(myCat.draw());
+ 
+
+    isColliding(character, platform);
     if (platform.y === character.y + character.w && platform.x <= character.x + character.w) {
         return true;
     } else {
         return false;
     }
-}
-}
