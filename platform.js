bcd0a2a9c63ee765fefc0c3241d6ff3158cce738
ngle25gd <ngle25gd@student.ju.se>
2025-12-15 16:07:29 +0100

    change platform

--- platform.js
+++ platform.js
@@ -13,9 +13,9 @@ draw() {
     let floatY = this.y + sin(frameCount * 0.02) * 5;
 
     fill(255, 230); 
-    ellipse(this.x + this.w * 0.2, this.y + this.h * 0.5, this.w * 0.4, this.h * 1.2);
-    ellipse(this.x + this.w * 0.5, this.y + this.h * 0.5, this.w * 0.6, this.h * 1.5);
-    ellipse(this.x + this.w * 0.8, this.y + this.h * 0.5, this.w * 0.4, this.h * 1.2);
+    ellipse(this.x + this.w * 0.2, floatY + this.h * 0.5, this.w * 0.4, this.h * 1.2);
+    ellipse(this.x + this.w * 0.5, floatY + this.h * 0.5, this.w * 0.6, this.h * 1.5);
+    ellipse(this.x + this.w * 0.8, floatY + this.h * 0.5, this.w * 0.4, this.h * 1.2);
 
 
     pop(); }
